7d61e2b4c46188ff18422f29f9c5e8845d2bb5f6
bjyfxuxiaojun <yfxuxiaojun@jd.com>
2017-10-22 22:52:51 +0800

    公告

--- src/test/groovy/CouponManageSpec.groovy
+++ src/test/groovy/CouponManageSpec.groovy
@@ -1,15 +1,19 @@
 import com.jd.pop.qa.FileRwTool
+import org.apache.commons.lang3.time.DateUtils
 import org.apache.http.HttpEntity
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
+import org.joda.time.DateTime
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import spock.lang.Shared
 import spock.lang.Specification
 
 import static com.jd.pop.qa.FileRwTool.readFromFile
+import static org.apache.commons.lang3.time.DateUtils.parseDate
 import static org.apache.http.impl.client.HttpClients.createDefault
 import static org.apache.http.util.EntityUtils.toString
 import static org.jsoup.Jsoup.parse
@@ -20,8 +24,6 @@ class CouponManageSpec extends Specification {
     @Shared
     String cookies = ""
 
-    @Shared
-    List<String> nendRecoverCouponIds = new ArrayList<>()
 
     @Shared
     def client = createDefault()
@@ -35,36 +37,21 @@ class CouponManageSpec extends Specification {
 
     def "恢复优惠券"() {
         Set<String> coupons = collectCoupons(700)
-
+        def pin = cookies.split(";").find { it.trim().startsWith("pin") }.split("=")[1]
         expect:
-        def httpGet = new HttpGet("https://quan.jd.com/delete_quan.action?page=7")
-        httpGet.setHeader("cookie", cookies)
-        def response = client.execute(httpGet)
-        def entity = response.getEntity()
-        def document = parse(toString(entity))
-
-        List<String> c_msgs = new ArrayList<>()
-        List<String> c_times = new ArrayList<>()
-
-        document.getElementsByClass("coupon-item coupon-item-j ").each {
-            it.getElementsByClass("c-msg").each {
-                c_msgs << it.getElementsByClass("txt").text().trim()
-            }
-        }
-
-        document.getElementsByClass("coupon-item coupon-item-j ").each {
-            c_times << it.getElementsByClass("c-time").text().trim()
-        }
-
-        c_msgs.collect {
-            it.split(" ")
+        coupons
+        coupons.every { couponId ->
+            def post = new HttpPost("https://quan.jd.com/unlock_coupon.action?pin=$pin&couponId=$couponId")
+            post.setHeader("cookie", cookies)
+            def response = client.execute(post)
+            response.statusLine.statusCode == 200
         }
 
     }
 
-    Set<String> collectCoupons(int i) {
-        def coupons = new HashSet<String>()
-        (1..i).each {
+    Set<String> collectCoupons(int pageNum) {
+        Set<String> coupons = new HashSet<String>()
+        (1..pageNum).each {
             def httpGet = new HttpGet("https://quan.jd.com/delete_quan.action?page=$it")
 
             httpGet.setHeader("cookie", cookies)
@@ -85,8 +72,32 @@ class CouponManageSpec extends Specification {
                 c_times << it.getElementsByClass("c-time").text().trim()
             }
 
+
+            c_msgs.eachWithIndex { String cMsg, int i ->
+                String[] msgLine = cMsg.split(" ")
+                def validEndDate = c_times[i]
+                Date date = parseDate(validEndDate, "yyyy.MM.dd");
+                boolean isValid = DateTime.now().isBefore(date.getTime()) || DateUtils.isSameDay(DateTime.now().toDate(), date);
+                if (belongShopSet(msgLine[0], msgLine) && msgLine[1] == "全平台" && isValid) {
+                    coupons.add(msgLine[2] as String)
+
+
+                }
+            }
         }
 
 
+        return coupons
+    }
+
+
+    private boolean belongShopSet(String shopName, Set<String> shopSet) {
+        for (String aShopSet : shopSet) {
+            if (shopName.contains(aShopSet)) {
+                return true
+            }
+
+        }
+        return false
     }
 }
