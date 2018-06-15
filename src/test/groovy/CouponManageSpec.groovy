ca08aba723678902fd62117fa77c3777a9443f96
bjyfxuxiaojun <yfxuxiaojun@jd.com>
2018-06-15 13:58:56 +0800

    添加按shop维度统计coupon列表和discout 添加阈值过滤列表 维护shop列表

--- src/test/groovy/CouponManageSpec.groovy
+++ src/test/groovy/CouponManageSpec.groovy
@@ -34,8 +34,8 @@ class CouponManageSpec extends Specification {
     }
 
 
-    def "删除后恢复"() {
-        Set<String> coupons = collectAllCoupons(10000)
+    def "check所有可恢复coupons"() {
+        Set<String> coupons = collectCoupons2delete(10000)
         String pin = getPinFromCookies(cookies)
         boolean deleteResult = coupons.isEmpty() ? true : executeDelete(coupons, pin)
 
@@ -43,7 +43,44 @@ class CouponManageSpec extends Specification {
         deleteResult
 
         when:
-        coupons = collectCoupons(2000)
+        coupons = collectRecoverableCoupons(70,2000)
+
+        then:
+        coupons
+        println "there are " + coupons.size() + " coupons can be recover."
+        couponPriceMap.each { entry -> println entry.key + "，优惠共计${entry.value}元" }
+    }
+
+    def "删除后恢复默认"() {
+        Set<String> coupons = collectCoupons2delete(10000)
+        String pin = getPinFromCookies(cookies)
+        boolean deleteResult = coupons.isEmpty() ? true : executeDelete(coupons, pin)
+
+        expect:
+        deleteResult
+
+        when:
+        coupons = collectRecoverableCoupons(100,1000)
+
+        then:
+        coupons
+        executeRecover(coupons, pin)
+
+        println "there are " + coupons.size() + " coupons have been recoved successfully."
+        couponPriceMap.each { entry -> println entry.key + "，优惠共计${entry.value}元" }
+
+
+    }
+    def "删除后恢复"() {
+        Set<String> coupons = collectCoupons2delete(10000)
+        String pin = getPinFromCookies(cookies)
+        boolean deleteResult = coupons.isEmpty() ? true : executeDelete(coupons, pin)
+
+        expect:
+        deleteResult
+
+        when:
+        coupons = collectCoupons2recover(2000)
 
         then:
         coupons
@@ -56,7 +93,7 @@ class CouponManageSpec extends Specification {
     }
 
     def "删除所有coupons"() {
-        Set<String> coupons = collectAllCoupons(10000)
+        Set<String> coupons = collectCoupons2delete(10000)
         String pin = getPinFromCookies(cookies)
 
         expect: "收集成功，并删除成功"
@@ -67,7 +104,7 @@ class CouponManageSpec extends Specification {
 
 
     def "恢复优惠券"() {
-        Set<String> coupons = collectCoupons(1000)
+        Set<String> coupons = collectCoupons2recover(1000)
         String pin = getPinFromCookies(cookies)
 
         expect: "收集成功，并恢复成功"
@@ -115,7 +152,7 @@ class CouponManageSpec extends Specification {
         }
     }
 
-    Set<String> collectAllCoupons(int pageNum) {
+    Set<String> collectCoupons2delete(int pageNum) {
         Set<String> coupons = new HashSet<String>()
         for (i in (1..pageNum)) {
             def httpGet = new HttpGet("https://quan.jd.com/user_quan.action?couponType=-1&sort=3&page=$i")
@@ -146,7 +183,7 @@ class CouponManageSpec extends Specification {
         return coupons
     }
 
-    Set<String> collectCoupons(int pageNum) {
+    Set<String> collectCoupons2recover(int pageNum) {
         Set<String> coupons = new HashSet<String>()
         for (i in (1..pageNum)) {
             def httpGet = new HttpGet("https://quan.jd.com/delete_quan.action?page=$i")
@@ -206,6 +243,81 @@ class CouponManageSpec extends Specification {
 
 
         return coupons
+    }
+
+    Set<String> collectRecoverableCoupons(int threshold, int pageNum) {
+        Set<String> coupons = new HashSet<String>()
+        Map<String, List<String>> shopCouponsMap = [:]
+        for (i in (1..pageNum)) {
+            def httpGet = new HttpGet("https://quan.jd.com/delete_quan.action?page=$i")
+            println "当前第$i 页……………………………………………………………………………………………………………………………………………………………………………………………………………………………………"
+            httpGet.setHeader("cookie", cookies)
+            def response = client.execute(httpGet)
+            def entity = response.getEntity()
+            def document = parse(toString(entity))
+
+            if (document.title().contains("京东-欢迎登录")) {
+                println document.title()
+                throw new RuntimeException("cookie无效，请重新登录！！！")
+            }
+
+
+            if (document.getElementsByClass("coupon-item").isEmpty()) {
+                println "当前页面无优惠券**************"
+                filterCoupons(threshold, shopCouponsMap, coupons)
+                return coupons
+
+            }
+
+
+            List<String> c_msgs = new ArrayList<>()
+            List<String> c_times = new ArrayList<>()
+            List<String> c_prices = new ArrayList<>()
+
+            document.getElementsByClass("coupon-item").each {
+                if (it.attributes().get("class").contains("coupon-item-j")
+                        || it.attributes().get("class").contains("coupon-item-myf")) {
+
+                    c_msgs << it.getElementsByClass("txt").text().trim()
+                    c_times << it.getElementsByClass("c-time").text().trim()
+                    c_prices << it.getElementsByClass("c-price").text().trim()
+                }
+
+            }
+
+            c_msgs.eachWithIndex { String cMsg, int j ->
+                String[] msgLine = cMsg.split("\\s+")
+//                String[] priceInfo = c_prices[j].split("\\s+")
+
+                String couponPrice = c_prices[j].replaceAll("\\D", "")
+                String validEndDate = c_times[j].replaceAll(".*-", "")
+                Date date = parseDate(validEndDate, "yyyy.MM.dd")
+                boolean isValid = DateTime.now().isBefore(date.getTime()) || DateUtils.isSameDay(DateTime.now().toDate(), date)
+                if (msgLine[1] == "全平台" && isValid) {
+                    println c_times[j] + cMsg
+//                    coupons.add(msgLine[2] as String)
+
+                    def shopCoupons = shopCouponsMap.containsKey(msgLine[0]) ? shopCouponsMap.get(msgLine[0]) << msgLine[2] : [msgLine[2]]
+                    shopCouponsMap.put(msgLine[0], shopCoupons)
+
+                    def totalPrice = couponPriceMap.containsKey(msgLine[0]) ? couponPriceMap.get(msgLine[0]) + Integer.valueOf(couponPrice) : Integer.valueOf(couponPrice)
+                    couponPriceMap.put(msgLine[0], totalPrice)
+                }
+            }
+        }
+
+        filterCoupons(threshold, shopCouponsMap, coupons)
+
+        return coupons
+    }
+
+    private filterCoupons(int threshold, LinkedHashMap<String, List<String>> shopCouponsMap, coupons) {
+        couponPriceMap = couponPriceMap.findAll { entry -> entry.value >= threshold }.sort { a, b -> b.value <=> a.value }
+        shopCouponsMap.each { entry ->
+            if (couponPriceMap.containsKey(entry.key)) {
+                coupons.addAll(entry.value)
+            }
+        }
     }
 
 
